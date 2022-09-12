b3d666d5fca9068a5e0fd7d7b9de62dbf8c914f1
paulkino <113321386+paulkino@users.noreply.github.com>
2022-09-12 00:41:45 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+package mx.utel.poo.intro;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        Mensaje msg = new Mensaje();
+        String nombre;
+        msg.saludar(nombre= "Paul");
+        }
+}
---
+++ Mensaje.java
@@ -0,0 +1,10 @@
+package mx.utel.poo.intro;
+
+public class Mensaje {
+
+    public void saludar(String nombre) {
+        System.out.printf("Hola %s bienvenido a prueba Java\n", nombre);
+    }
+
+
+}
